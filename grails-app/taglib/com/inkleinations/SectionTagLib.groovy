a8e143c489e3c344d019b26e8b4bbc0998dfedce
Benjamin Klein <ben@silver-chalice.com>
2013-01-31 16:14:58 -0600

    add image tag to SectionTagLib

--- grails-app/taglib/com/inkleinations/SectionTagLib.groovy
+++ grails-app/taglib/com/inkleinations/SectionTagLib.groovy
@@ -8,4 +8,12 @@ class SectionTagLib {
         out << Section.findByName(attrs.for)?.html ?: ""
     }
 
+    def image = { attrs ->
+        def section = Section.findByName(attrs.for)
+        if(section){
+            def src = g.resource(dir: attrs.dir ? 'images' + '/' + attrs.dir : 'images', file: section.html)
+            out << '<img src="' + src + '" width="' + attrs.width + '" height="' + attrs.height + '" alt="' + attrs.alt + '" />'
+        }
+    }
+
 }
