813e46de42cbc0e36547d484c804b59389de6c09
Benjamin Klein <ben@silver-chalice.com>
2013-02-08 20:45:46 -0600

    add email functionality to contact action

--- grails-app/controllers/com/inkleinations/HomeController.groovy
+++ grails-app/controllers/com/inkleinations/HomeController.groovy
@@ -1,6 +1,10 @@
 package com.inkleinations
 
+import org.apache.commons.validator.EmailValidator 
+
 class HomeController {
+
+    def mailService
 
     def index() { }
 
@@ -27,6 +31,29 @@ class HomeController {
 
     def links() { }
 
-    def contact() { }
+    def contact() {
+        def msg = null
+        def dateFormat = "EEEE, MMMM d, yyyy, 'at' h:mm a"
+        EmailValidator emailValidator = EmailValidator.getInstance() 
+        if(request.method == "POST"){
+            if(params.message && params.email){
+                mailService.sendMail {
+                    to "daveanddebklein@yahoo.com"
+                    from "inKLEINations.com <inkleinationswebservant@gmail.com>"
+                    //bcc "ben@silver-chalice.com"
+                    subject "${params.subject} (from inKLEINations.com)"
+                    html "<p>On ${g.formatDate(date:new Date(), format: dateFormat)}, ${params.email} said:</p> <p>-----------------------------------------------------------</p> ${params.message.encodeAsHTML()} <p>------------------------------------------------------------</p><p>This is an automated message. Please do not reply to this email. If you need additional help, visit <a href='http://www.apple.com/support/appleid/'>Apple Support</a>.</p>"
+                 }
+                 msg = "Thank you! Your message has been sent."
+            } else if(!params.email){
+                msg = "Please enter your email address so that we can respond."
+            } else if(params.email && !emailValidator.isValid(params.email)){
+                msg = "Please enter a valid email address so that we can respond."
+            } else {
+                msg = "Please enter a message to send."
+            }
+        }
+        [message: msg]
+    }
 
 }
